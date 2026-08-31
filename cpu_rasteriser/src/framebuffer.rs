b64adac3ca778140b3a27f6b220c9c3930433632
claire <claire.stott@everbit.dev>
2026-08-31 14:10:36 +0100

    Add SIMD blending

--- cpu_rasteriser/src/framebuffer.rs
+++ cpu_rasteriser/src/framebuffer.rs
@@ -220,4 +220,26 @@ impl FrameBuffer {
         ];
         f32x8::new(values)
     }
+
+    #[inline(always)]
+    pub unsafe fn get8_unchecked(&self, index: usize) -> ColourSimd {
+        debug_assert!(index + 8 <= self.pixels.len());
+
+        // TODO: look into different storage layout of framebuffer to make this more efficient, e.g. SoA instead of AoS
+        let p0 = unsafe { self.pixels.get_unchecked(index) };
+        let p1 = unsafe { self.pixels.get_unchecked(index + 1) };
+        let p2 = unsafe { self.pixels.get_unchecked(index + 2) };
+        let p3 = unsafe { self.pixels.get_unchecked(index + 3) };
+        let p4 = unsafe { self.pixels.get_unchecked(index + 4) };
+        let p5 = unsafe { self.pixels.get_unchecked(index + 5) };
+        let p6 = unsafe { self.pixels.get_unchecked(index + 6) };
+        let p7 = unsafe { self.pixels.get_unchecked(index + 7) };
+
+        ColourSimd {
+            r: f32x8::new([p0.r, p1.r, p2.r, p3.r, p4.r, p5.r, p6.r, p7.r]),
+            g: f32x8::new([p0.g, p1.g, p2.g, p3.g, p4.g, p5.g, p6.g, p7.g]),
+            b: f32x8::new([p0.b, p1.b, p2.b, p3.b, p4.b, p5.b, p6.b, p7.b]),
+            a: f32x8::new([p0.a, p1.a, p2.a, p3.a, p4.a, p5.a, p6.a, p7.a]),
+        }
+    }
 }
